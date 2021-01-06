11e6589f6dd54567e62077226ba78ab202fd678b
Tu Nombre <montespierre@gmail.com>
2021-01-05 23:22:07 -0500

    primera subida

---
+++ Shipibo/src/java/com/weimont/config/Conexion.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.weimont.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+/**
+ *
+ * @author montespierreg
+ */
+public class Conexion {
+    
+    private Conexion(){
+        
+    }
+
+    public static Connection conexion() throws SQLException, InstantiationException, IllegalAccessException {
+		Connection cn = null;
+
+		// Parametros
+		// String direccion =
+		// "jdbc:mariadb://localhost:3306/agenda?user=moche232&password=2322715";
+		String driver = "org.mariadb.jdbc.Driver";
+		String urlDB = "jdbc:mariadb://localhost:3306/agenda";
+		String user = "admin";
+		String pass = "";
+
+		try {
+			//Class.forName("org.mariadb.jdbc.Driver").newInstance();
+			Class.forName(driver).newInstance();
+
+			// cn = DriverManager.getConnection(direccion);
+			cn = DriverManager.getConnection(urlDB, user, pass);
+
+		} catch (SQLException e) {
+			throw e;
+		} catch (ClassNotFoundException e) {
+			throw new SQLException("No se encontro el driver de la BD");
+		} catch (Exception e) {
+			throw new SQLException("No se puede establecer conexion de la BD");
+		}
+		return cn;
+	}
+}
---
+++ Shipibo/web/Principal.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : Principal.jsp
+    Created on : 05/01/2021, 10:32:51 PM
+    Author     : montespierreg
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
