c3a06b1aed94a42e780fc3e04861b8c662847d09
unknown <montespierre@gmail.com>
2021-02-10 20:23:55 -0500

    otro

--- Shipibo/src/java/com/weimont/config/Conexion.java
+++ Shipibo/src/java/com/weimont/config/Conexion.java
@@ -29,8 +29,8 @@ public class Conexion {
 		// "jdbc:mariadb://localhost:3306/agenda?user=moche232&password=2322715";
 		String driver = "org.mariadb.jdbc.Driver";
 		String urlDB = "jdbc:mariadb://localhost:3306/shipibo";
-		String user = "ventas";
-		String pass = "2322715";
+		String user = "root";
+		String pass = "";
 
 		try {
 			//Class.forName("org.mariadb.jdbc.Driver").newInstance();
